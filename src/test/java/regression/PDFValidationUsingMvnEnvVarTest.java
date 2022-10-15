145000c562ea432036507135a7acb0aefba9500f
bheemesh <manu.bheemesh@gmail.com>
2022-10-16 01:37:48 +0530

    added pdf tests and code based on environment variables

--- src/test/java/regression/PDFValidationUsingMvnEnvVarTest.java
+++ src/test/java/regression/PDFValidationUsingMvnEnvVarTest.java
@@ -9,18 +9,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.PDFUtilities;
 
-public class PDFValidationNegetiveTest {
+import java.util.Properties;
+
+public class PDFValidationUsingMvnEnvVarTest {
     WebDriver driver;
     String pdfPath;
     PDFUtilities pdfUtilities;
 
     @BeforeMethod
-    public void setup() {
+    public void setup() throws Exception {
 
+        Properties properties = new Properties();
+        properties.load(PDFValidationUsingMvnEnvVarTest.class.getClassLoader().
+                getResourceAsStream("config.properties"));
+        String pathway = properties.getProperty("path");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("file:///C:/Users/lenovo/Desktop/Bheemesh_Resume.pdf");
+        driver.get(pathway);
         pdfPath = driver.getCurrentUrl();
         pdfUtilities = new PDFUtilities();
 
@@ -33,8 +39,7 @@ public class PDFValidationNegetiveTest {
         String pdfData = pdfUtilities.getPDFfiledata(pdfPath);
         System.out.println("hey iam here  PDF data >>>>>>>>>>>>");
         System.out.println(pdfData);
-        Assert.assertFalse(pdfData.contains("HackThon123"));
-        System.out.println("successfully asserted negitive case also");
+        Assert.assertTrue(pdfData.contains("HackThon"));
 
 
     }
