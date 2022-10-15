502bd9c5a4611059b3b35e52fdfc312a88481343
bheemesh <manu.bheemesh@gmail.com>
2022-10-15 21:24:37 +0530

    added negetive testcases

---
+++ src/test/java/regression/PDFValidationNegetiveTest.java
@@ -0,0 +1,49 @@
+package regression;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.PDFUtilities;
+
+public class PDFValidationNegetiveTest {
+    WebDriver driver;
+    String pdfPath;
+    PDFUtilities pdfUtilities;
+
+    @BeforeMethod
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///C:/Users/lenovo/Desktop/Bheemesh_Resume.pdf");
+        pdfPath = driver.getCurrentUrl();
+        pdfUtilities = new PDFUtilities();
+
+    }
+
+    @Test
+    public void pdfTest() throws Exception {
+
+        System.out.println("hey i try open PDF File for you and located at >>>>>" + pdfPath);
+        String pdfData = pdfUtilities.getPDFfiledata(pdfPath);
+        System.out.println("hey iam here  PDF data >>>>>>>>>>>>");
+        System.out.println(pdfData);
+        Assert.assertTrue(!pdfData.contains("HackThon123"));
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+
+    }
+
+
+}
